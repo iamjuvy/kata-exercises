03074d4fe1300bebabecb6a357e179c6407f64c8
iamjuvy <iamjuvy@gmail.com>
2019-03-25 18:42:28 -0500

    test for valid input

--- babysitter/src/main/java/BabySitter.java
+++ babysitter/src/main/java/BabySitter.java
@@ -1,6 +1,51 @@
 public class BabySitter {
 
+    //time format accepted: xx:yy or xx
+    public boolean validTimeFormat(String time) {
+        String[] timeParts = time.split(":");
+        System.out.println("timePartsLength: " + timeParts.length);
+        Integer hour = 0, min=0;
+        if (timeParts.length!=0 && timeParts.length <=2 ){
+            try{
+                hour = Integer.parseInt(timeParts[0]);
+                if (hour==0 || hour>12){
+                    return false;
+                }
+
+                if (timeParts.length==2){
+                    min = Integer.parseInt(timeParts[1]);
+
+                    if (min==0 || min >59){
+                        return false;
+                    }
+                }
+            }catch(NumberFormatException e){
+                //e.printStackTrace();
+                return false;
+            }
+
+        }else{
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param startTime 10:30 or 10
+     * @param endTime
+     * @return
+     */
     public int computeFamilyA(String startTime, String endTime) {
 
+
+
+        int startime = Integer.parseInt(startTime);
+        int endtime = Integer.parseInt(endTime);
+
+        return 0;
+
     }
+
+
 }
--- babysitter/src/test/java/BabySitterTest.java
+++ babysitter/src/test/java/BabySitterTest.java
@@ -9,7 +9,21 @@ class BabySitterTest {
     }
 
     @Test
-    public void computeFamilyA(String startTime, String endTime){
+    public void testValidTimeFormat(){
+        BabySitter bSitter = new BabySitter();
+        assertTrue(bSitter.validTimeFormat("1"));
+        assertTrue(bSitter.validTimeFormat("10"));
+        assertTrue(bSitter.validTimeFormat("1:30"));
+        assertTrue(bSitter.validTimeFormat("10:30"));
+        assertFalse(bSitter.validTimeFormat("130"));
+        assertFalse(bSitter.validTimeFormat("13:30"));
+        assertFalse(bSitter.validTimeFormat("11:60"));
+        assertFalse(bSitter.validTimeFormat("abc"));
+        assertFalse(bSitter.validTimeFormat("ab:cd"));
+    }
+
+    @Test
+    public void testComputeFamilyA(){
         BabySitter bSitter = new BabySitter();
         assertEquals(30, bSitter.computeFamilyA("10", "1"));
     }
